34a536f067b291b6dd1ee3bec3a772fc7c3d7e68
jcfun <jcfunstar@gmail.com>
2023-04-07 07:33:13 +0800

    daily update

---
+++ web-client/mini-program/src/components/mine-login/mine-login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    用户登录
+  </div>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<style scoped lang='scss'>
+
+</style>
---
+++ web-client/mini-program/src/pages/mine/mine.vue
@@ -0,0 +1,15 @@
+<template>
+  <view>
+    <mine-login v-if="user.token"></mine-login>
+    <mine-userinfo v-else></mine-userinfo>
+  </view>
+</template>
+
+<script setup lang='ts'>
+import { useUserStore } from '../../store/user'
+
+const user = useUserStore()
+
+</script>
+
+<style scoped lang='scss'></style>
